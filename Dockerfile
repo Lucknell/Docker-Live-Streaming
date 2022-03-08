6e55d317f4de848bcee0abb7f2f573130366e7af
Lucknell <Lucknell@email.com>
2022-03-08 06:51:17 +0000

    Fixed the content server not starting

--- Dockerfile
+++ Dockerfile
@@ -5,6 +5,7 @@ RUN apt install -y nginx libnginx-mod-rtmp
 RUN cd /mnt && mkdir hls && chown www-data:www-data hls
 WORKDIR /mnt
 COPY index.html /var/www/html/index.html
+COPY content.conf /content.conf
 RUN chmod 644 /var/www/html/index.html
 COPY ./docker_entrypoint.sh /
 RUN chmod +x /docker_entrypoint.sh
--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -1,35 +1,36 @@
 #!/bin/bash
 
-sed -i "s/https:\/\/mydomain.com\/hls\/STREAM_KEY.m3u8/${CONTENT_URL}\/hls\/${STREAM_KEY}.m3u8/" /var/www/html/index.html
+sed -i "s~https:\/\/mydomain.com\/hls\/STREAM_KEY.m3u8~${CONTENT_URL}\/hls\/${STREAM_KEY}.m3u8~" /var/www/html/index.html
 
 RTMP=$(grep "rtmp" /etc/nginx/nginx.conf)
 
 if [ -z "$RTMP" ];then #first run fill in file
   echo "day 0 config running"
+  sed -i "/include \/etc\/nginx\/sites-enabled\//r /content.conf" /etc/nginx/nginx.conf
   cat >> /etc/nginx/nginx.conf << EOF
 
-  rtmp {
-          server {
-                  listen 1935;
-                  chunk_size 4096;
-  
-                  application live {
-                  live on;
-                  record off;
-  
-                  allow publish ${SERVER_IP};
-                  #deny publish all;
-  
-                  # Turn on HLS
-                  hls on;
-                  hls_path /mnt/hls/;
-                  hls_fragment 3;
-                  hls_playlist_length 60;
-                  # disable consuming the stream from nginx as rtmp
-                  deny play all;
-                  }
-          }
-  }
+rtmp {
+        server {
+                listen 1935;
+                chunk_size 4096;
+
+                application live {
+                live on;
+                record off;
+
+                allow publish ${SERVER_IP};
+                #deny publish all;
+
+                # Turn on HLS
+                hls on;
+                hls_path /mnt/hls/;
+                hls_fragment 3;
+                hls_playlist_length 60;
+                # disable consuming the stream from nginx as rtmp
+                deny play all;
+                }
+        }
+}
 EOF
 fi
 
